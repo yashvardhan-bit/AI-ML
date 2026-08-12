2ad9bff533e12bd37ca175a5a28298e7a5e35a94
yashvardhan-bit <yashachlawat@gmail.com>
2026-08-12 23:53:04 +0530

    file handling

--- Python/file.py
+++ Python/file.py
@@ -14,3 +14,19 @@ with open("sample.txt","r") as f:
             print("present in line ", line)
             break  
         line+=1
+f=open("sample.txt","w")
+f.write("all the data is overwritten")
+f.close()
+with open("sample.txt","r") as f:
+    print(f.read())
+import os
+os.remove("sample2.txt")
+data=True
+line=1
+with open("sample.txt","r") as f:
+    while data:
+        data=f.readline()
+        if "python" in data:
+            print("present in line ", line)
+            break  
+        line+=1
